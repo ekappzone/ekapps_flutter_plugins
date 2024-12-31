43285729caac627f7308957c3a1e16c9ee56a9dd
Kasun <ekappzone@gmail.com>
2024-12-31 11:15:20 +0530

    Ranga from ekappzone added two widgets

---
+++ ekappz_widgets/lib/widgets/ekappz_bannerAd_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+class BannerAdWidget extends StatefulWidget {
+  const BannerAdWidget({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _BannerAdWidgetState createState() => _BannerAdWidgetState();
+}
+
+class _BannerAdWidgetState extends State<BannerAdWidget> {
+  BannerAd? _bannerAd;
+  bool _isAdLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _bannerAd = BannerAd(
+      adUnitId:
+      'ca-app-pub-3940256099942544/9214589741', // Replace with your Ad Unit ID
+      size: AdSize.banner,
+      request: const AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (_) {
+          setState(() {
+            _isAdLoaded = true;
+          });
+        },
+        onAdFailedToLoad: (ad, error) {
+          ad.dispose();
+        },
+      ),
+    )..load();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _isAdLoaded
+        ? Container(
+      alignment: Alignment.center,
+      width: _bannerAd!.size.width.toDouble(),
+      height: _bannerAd!.size.height.toDouble(),
+      child: AdWidget(ad: _bannerAd!),
+    )
+        : const SizedBox.shrink();
+  }
+
+  @override
+  void dispose() {
+    _bannerAd?.dispose();
+    super.dispose();
+  }
+}
---
+++ ekappz_widgets/lib/widgets/ekappz_drawer.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class DrawerPAge extends StatelessWidget {
+  const DrawerPAge({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    return Drawer(
+      child: Container(
+        decoration:
+        const BoxDecoration(color: Color.fromARGB(231, 255, 203, 112)),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(
+                    255, 255, 223, 141), // Background color of the DrawerHeader
+              ),
+              child: Row(
+                children: [
+                  const CircleAvatar(
+                    radius: 30, // Radius of the avatar
+                    backgroundImage: AssetImage(
+                        'assets/logo.png'), // Replace with your image asset
+                  ),
+                  const SizedBox(width: 16), // Space between avatar and text
+                  Text(
+                    'EK AppZone',
+                    style: TextStyle(
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                      fontSize: screenWidth * 0.07,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              title: Text(
+                "EK App Zone is a forward-thinking digital solution company  in Sri Lanka, dedicated to delivering innovative app solutions. Our expertise lies in creating cutting-edge applications that cater to the evolving needs of businesses and individuals. We combine technology with creativity to offer products that enhance efficiency, connectivity, and user experience.",
+                style: TextStyle(
+                    fontWeight: FontWeight.w800, fontSize: screenWidth * 0.03),
+              ),
+              onTap: () {
+                // Handle item 1 tap
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.email),
+              title: const Text('Ekappzone@gmail.com'),
+              onTap: () {
+                // Handle item 2 tap
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.phone),
+              title: const Text('+94782694957'),
+              onTap: () {
+                // Handle item 2 tap
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.location_city),
+              title: const Text("Nuwaraeliya,Srilanka"),
+              onTap: () {
+                // Handle item 2 tap
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+            // Add more ListTile items as needed
+          ],
+        ),
+      ),
+    );
+  }
+}
