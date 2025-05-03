cd2556eedc17965535e31f9c6d44ba3aea76abfb
Prabhashwara Ekanayake <prabhashwaraekanayake@gmail.com>
2025-05-03 23:19:18 +0530

    Update ekapps_flutter_plugins.dart
    ad manager implementation

--- lib/ekapps_flutter_plugins.dart
+++ lib/ekapps_flutter_plugins.dart
@@ -2,4 +2,5 @@ library ekapps_flutter_plugins;
 
 export 'widgets/ekappz_bannerAd_widget.dart';
 export 'widgets/ekappz_drawer.dart';
+export 'widgets/ekappz_ad_manager.dart';
 // Add more exports as needed
